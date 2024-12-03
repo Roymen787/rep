b71dd87762d2d4f5efe39ad4dcb41962c71abd80
Roman_1 <af@.ru>
2024-12-03 16:29:19 +0300

    color Circles

--- Smsl_jisni.py
+++ Smsl_jisni.py
@@ -3,6 +3,32 @@ import random
 from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QWidget, QVBoxLayout
 from PyQt6.QtGui import QPainter, QColor
 
+
+class Circle:
+    def __init__(self, diameter):
+        self.diameter = diameter
+        self.color = QColor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
+
+class Circle_Drawer(QWidget):
+    def __init__(self):
+        super(CircleDrawer, self).__init__()
+        self.circles = []
+
+    def update_canvas(self, diameter):
+        new_circle = Circle(diameter)
+        self.circles.append(new_circle)
+        self.update()
+
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        for circle in self.circles:
+            x = random.randint(0, self.width() - circle.diameter)
+            y = random.randint(0, self.height() - circle.diameter)
+            painter.setBrush(circle.color)
+            painter.drawEllipse(x, y, circle.diameter, circle.diameter)
+
+
 class MainApp(QMainWindow):
     def __init__(self):
         super(MainApp, self).__init__()
@@ -10,30 +36,20 @@ class MainApp(QMainWindow):
         self.setWindowTitle("Random Circles Application")
         self.setGeometry(100, 100, 800, 600)
 
-        self.central_widget = QWidget()
-        self.setCentralWidget(self.central_widget)
+        self.circle_drawer = CircleDrawer()
+        self.setCentralWidget(self.circle_drawer)
 
         self.layout = QVBoxLayout()
-        self.central_widget.setLayout(self.layout)
+        self.circle_drawer.setLayout(self.layout)
 
         self.button = QPushButton("Добавить окружность")
-        self.button.clicked.connect(self.update_canvas)
+        self.button.clicked.connect(self.add_circle)
         self.layout.addWidget(self.button)
 
-        self.circles = []
-
-    def update_canvas(self):
+    def add_circle(self):
         diameter = random.randint(20, 100)
-        self.circles.append(diameter)
-        self.update()
+        self.circle_drawer.update_canvas(diameter)
 
-    def paintEvent(self, event):
-        painter = QPainter(self)
-        painter.setBrush(QColor(255, 255, 0))
-        for diameter in self.circles:
-            x = random.randint(0, self.width() - diameter)
-            y = random.randint(0, self.height() - diameter)
-            painter.drawEllipse(x, y, diameter, diameter)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
