27588b8c51d98046509c172ba3a94af975a7b2eb
Roman_1 <af@.ru>
2024-12-03 16:11:02 +0300

    Yellow Circles

---
+++ ia_ne_lox.py
@@ -0,0 +1,45 @@
+# Проблемы с подключением в GitHub
+
+import sys
+import random
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QWidget, QVBoxLayout
+from PyQt6.QtGui import QPainter, QColor
+from PyQt6.QtCore import Qt
+
+class MainApp(QMainWindow):
+    def __init__(self):
+        super(MainApp, self).__init__()
+
+        self.setWindowTitle("Random Circles Application")
+        self.setGeometry(100, 100, 800, 600)
+
+        self.central_widget = QWidget()
+        self.setCentralWidget(self.central_widget)
+
+        self.layout = QVBoxLayout()
+        self.central_widget.setLayout(self.layout)
+
+        self.button = QPushButton("Добавить окружность")
+        self.button.clicked.connect(self.update_canvas)
+        self.layout.addWidget(self.button)
+
+        self.circles = []
+
+    def update_canvas(self):
+        diameter = random.randint(20, 100)
+        self.circles.append(diameter)
+        self.update()
+
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        painter.setBrush(QColor(255, 255, 0))
+        for diameter in self.circles:
+            x = random.randint(0, self.width() - diameter)
+            y = random.randint(0, self.height() - diameter)
+            painter.drawEllipse(x, y, diameter, diameter)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = MainApp()
+    window.show()
+    sys.exit(app.exec())
